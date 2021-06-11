aa5d14c7f2b58697cda317af420edccff9588f67
Jules Scheil <juliana.m.scheil@gmail.com>
2021-06-11 11:59:02 -0400

    dynamically showing cards, no longer hard coded

--- src/components/Timeslots.js
+++ src/components/Timeslots.js
@@ -3,36 +3,21 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
+
+
 // Inserting data & time cards
 function Timeslots() {
+
+    const hours=[9,10,11,12,1,2,3,4]
+    const hourValue=[];
+    for(const value of hours) {
+        hourValue.push(<Card><Card.Body>{value}-{value+1}</Card.Body></Card>)
+    }
+
 return (
     <div className="container">
         <h2>times available</h2>
-        {/* hard coded, make dynamic later */}
-        <Card>
-            <Card.Body>9-10</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>10-11</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>11-12</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>12-1</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>1-2</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>2-3</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>3-4</Card.Body>
-        </Card>
-        <Card>
-            <Card.Body>4-5</Card.Body>
-        </Card>
+            {hourValue}
     </div>
 )
 };
