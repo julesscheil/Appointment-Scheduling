9aa23f1042638b08d053a693911fb612e124ceab
Jules Scheil <juliana.m.scheil@gmail.com>
2021-06-11 11:03:52 -0400

    added header and timeslot component structure, set up Header in App & is displaying

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// Component for the header
+
+import React from 'react';
+
+// This will serve as the JSX inserted in the header
+function Header() {
+return (
+    <div className="Container">
+        <h1> Hello Header </h1>
+    </div>
+)
+
+};
+
+export default Header;
